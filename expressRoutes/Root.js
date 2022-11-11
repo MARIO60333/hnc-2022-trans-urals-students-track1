5a9227720749dfdc3e2ce939b50656e81eb6009f
MARIO60333 <bulatov.bul58@gmail.com>
2022-11-12 05:26:58 +1000

    Linked back and front, added static route

--- expressRoutes/Root.js
+++ expressRoutes/Root.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 router.use(function timeLog(req, res, next) {
   console.log("Time ROOT: ", Date.now());
@@ -7,7 +8,7 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get("/", function (req, res) {
-  res.send("Home Page!");
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const port = process.env.PORT || 5000;
 
 const root = require("./expressRoutes/Root");
@@ -9,6 +10,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/", root);
 app.use("/api", api);
