0860bcc0d378f812643bf22c1e481abdecca6995
ded-insult <jkhgwa879y@gmail.com>
2022-11-12 05:05:50 +1000

    added test to main page

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,15 @@ import Home from "./components/Home/Home";
 import Header from "./components/UI/Header";
 
 const App = () => {
+
+    // fetch('http:/localhost:5000')
+    //     // .then(res => res.json())
+    //     .then(com => console.log(com))
+
     return (
         <div className="app">
             <Header/>
+
             <Home/>
         </div>
     );
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-
+            <h2>i it is main page</h2>
         </div>
     );
 };
