3c3240ef0627caf954338f48ed95c23f9c012db9
ded-insult <jkhgwa879y@gmail.com>
2022-11-12 04:25:08 +1000

    first commit

---
+++ client/src/components/Streamer/Streamer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Streamer = () => {
+    return (
+        <div>
+            streamer
+        </div>
+    );
+};
+
+export default Streamer;
---
+++ client/src/components/UI/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Header.module.css'
+const Header = () => {
+    return (
+        <div>
+            <div className={s.row}>
+                <ul>
+                    <li>saskksal </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
