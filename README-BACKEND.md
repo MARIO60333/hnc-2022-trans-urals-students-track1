882f4bbda1137ed93858e9bcc29931828b5c7865
MARIO60333 <bulatov.bul58@gmail.com>
2022-11-12 23:41:01 +1000

    Added ``/singus`` API route

--- README-BACKEND.md
+++ README-BACKEND.md
@@ -16,6 +16,30 @@ JSON:
 }
 ```
 
+## `/postping`
+
+### Запрос:
+
+POST:
+* ``msg``: сообщение
+
+### Пример: 
+```json
+{
+  "msg": "<msg>"
+}
+```
+
+### Ответ:
+
+JSON:
+
+```json
+{
+  "msg":"GET Pong: <msg>"
+}
+```
+
 ## `/getping`
 
 ### Запрос:
@@ -29,10 +53,44 @@ GET:
 
 ### Ответ:
 
-JSON:
+      JSON:
 
 ```json
 {
   "msg":"GET Pong: <msg>"
+}
+```
+
+## `/singus`
+Регистрация стримера
+
+### Запрос:
+
+POST:
+
+* ``username``: Никнейм
+* ``firstName``: Имя
+* ``lastName``: Фамилия
+* ``password``: Пароль
+* ``phoneNumber``: Номер телефона
+
+### Пример: 
+```json
+{
+  "username": "<UserName>",
+  "firstName": "Вася",
+  "lastName": "Пупкин",
+  "password": "<Password>",
+  "phoneNumber": "+7xxxxxxxxxx"
+}
+```
+
+### Ответ:
+
+JSON:
+
+```json
+{
+  "status": 0
 }
 ```
--- expressRoutes/API.js
+++ expressRoutes/API.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const axios = require("axios");
+const { MongoClient } = require("mongodb");
 const router = express.Router();
+
+const mongo_url = process.env.MONGO_URL || "mongodb://0.0.0.0:27017";
 
 router.use(function timeLog(req, res, next) {
   console.log("Time API: ", Date.now());
@@ -19,9 +23,137 @@ router.get("/getping", function (req, res) {
 });
 
 router.post("/postping", function (req, res) {
-    res.json({
-      msg: `POST Pong: ${req.body.msg}`
-    })
+  res.json({
+    msg: `POST Pong: ${req.body.msg}`,
   });
+});
+
+// router.post("/singus", function (req, res) {
+//   axios({
+//     method: "post",
+//     url:
+//       process.env.AXIOS_ALFA_URL + "/api/external/v1/waiter/registration/data",
+//     headers: { Authorization: process.env.AXIOS_ALFA_TOKEN },
+//     data: {
+//       codeId: null,
+//       sector: "tip",
+//       isDuplicateCodesAllowed: null,
+//       groupId: process.env.AXIOS_ALFA_GROUP_ID,
+//       firstName: req.body.firstName,
+//       lastName: req.body.lastName,
+//       phoneNumber: req.body.phoneNumber,
+//     },
+//   }).then(function (response) {
+//     let client = new MongoClient(mongo_url);
+
+//     async function run() {
+//       try {
+//         const db = client.db("baseUser");
+//         const users = db.collection("users");
+
+//         const checkUserName = await users.findOne({
+//           username: req.body.username,
+//         });
+//         if (checkUserName == null) {
+//           const doc = {
+//             codeId: response.data.code,
+//             username: req.body.username,
+//             firstName: req.body.firstName,
+//             lastName: req.body.lastName,
+//             password: req.body.password,
+//             phoneNumber: req.body.phoneNumber,
+//           };
+
+//           const result = await users.insertOne(doc);
+
+//           console.log(
+//             `A document was inserted with the _id: ${result.insertedId}`
+//           );
+//         } else {
+//           throw {name: 1, message: "This username is exist!"}
+//           await client.close();
+//           return;
+//         }
+//       } finally {
+//         await client.close();
+//       }
+//     }
+//     run().catch( e => {
+//       throw e
+//     });
+//   });
+
+//   res.json({
+//     status: 0,
+//   });
+// });
+
+router.post("/singus", function (req, res) {
+  const client = new MongoClient(mongo_url);
+
+  console.log("[SingUs] MongoDB client opened (init)");
+
+  async function run() {
+    try {
+      const db = client.db("baseUser");
+      const users = db.collection("users");
+
+      const checkUserName = await users.findOne({
+        username: req.body.username,
+      });
+
+      if (checkUserName == null) {
+        const doc = {
+          username: req.body.username,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          password: req.body.password,
+          phoneNumber: req.body.phoneNumber,
+        };
+
+        await axios({
+          method: "post",
+          url:
+            process.env.AXIOS_ALFA_URL +
+            "/api/external/v1/waiter/registration/data",
+          headers: { Authorization: process.env.AXIOS_ALFA_TOKEN },
+          data: {
+            codeId: null,
+            sector: "tip",
+            isDuplicateCodesAllowed: null,
+            groupId: process.env.AXIOS_ALFA_GROUP_ID,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNumber: req.body.phoneNumber,
+          },
+        }).then(function (response) {
+          doc.nm_codeId = response.data.code;
+        });
+
+        const result = await users.insertOne(doc);
+
+        console.log(
+          `baseUser DB > users collection > _id: ${result.insertedId} inserted`
+        );
+
+        res.json({
+          status: 0
+        });
+      } else {
+        throw { name: 1, message: "This Username is exist!" };
+      }
+    } catch (e) {
+      res.status(500).json({
+        status: e.name,
+        msg: e.message,
+      });
+      return;
+    } finally {
+      client.close();
+      console.log("[SingUs] MongoDB client closed (init)");
+    }
+  }
+  run();
+});
 
 module.exports = router;
